5acf38c594e80042f1cc7867a67a3fc53bcee1c0
sogandsa95 <sogand.sa95@gmail.com>
2025-03-14 16:36:13 +0330

    Refactor Home component into a separate module and enhance styling with responsive design

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,5 @@
-import { Col, Flex } from "antd";
-import { HomeContainer } from "./styles";
-import Image from "next/image";
+import Home from "@/modules/Home";
 
-export default function Home() {
-  return (
-    <HomeContainer justify={"center"}>
-      <Col span={24}>
-        <Image
-          src="/images/iranMap.svg"
-          alt="iranMap"
-          width={381}
-          height={350}
-        />
-      </Col>
-      <Col span={24}>
-        <p className="fontHolder">
-          لطفا نوع نمایش تحلیل مد نظر خود را انتخاب کنید.
-        </p>
-      </Col>
-      <Col span={24}>
-        <Flex gap={40}>
-          <Flex align="center" vertical className="itemHolder">
-            <Image src="/images/map.svg" alt="iranMap" width={40} height={40} />
-            <div className="itemText">نقشه</div>
-          </Flex>
-          <Flex align="center" vertical className="itemHolder">
-            <Image
-              src="/images/table.svg"
-              alt="iranMap"
-              width={40}
-              height={40}
-            />
-            <div className="itemText">جدول</div>
-          </Flex>
-          <Flex align="center" vertical className="itemHolder">
-            <Image
-              src="/images/card.svg"
-              alt="iranMap"
-              width={40}
-              height={40}
-            />
-            <div className="itemText">کارت</div>
-          </Flex>
-        </Flex>
-      </Col>
-    </HomeContainer>
-  );
+export default function HomePage() {
+  return <Home />;
 }
