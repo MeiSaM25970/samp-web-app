97951487c144a456f72d9384b86babfe75ce1e78
MeiSaM25970 <meisam25970@gmail.com>
2025-03-14 18:10:51 +0330

    favicon title

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,24 +21,8 @@ export default async function RootLayout({
   return (
     <html lang="fa" className="overflow-x-hidden">
       <head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/img/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/img/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/img/favicon/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/img/favicon/site.webmanifest" />
+        <link rel="icon" href="/images/favicon.svg" sizes="any" />
+        <title>Samp</title>
       </head>
 
       <BodyWrapper>
